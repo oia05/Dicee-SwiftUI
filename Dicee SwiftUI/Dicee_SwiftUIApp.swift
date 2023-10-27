1b3f175cf92a858344d4f9de441591e70e85814f
omar assidi <omar.assidi@cmeoffshore.com>
2023-10-27 15:35:36 +0300

    added project

---
+++ Dicee SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee SwiftUI
+//
+//  Created by OmarAssidi on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
